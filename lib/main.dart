bbf2aa61068d6dad8c358e884f23944983016ab5
Ashutosh_Rai_78 <ashutosh.devel0oper78@gmail.com>
2022-06-05 03:48:29 -0400

    routes DONE

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
+import 'package:firebase_email_password_login/login_page.dart';
+import 'package:firebase_email_password_login/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'login',
+    routes: {
+      'login' : (context) => Login(),
+      'register' : (context) => Register()
+    },
+  ));
 }
